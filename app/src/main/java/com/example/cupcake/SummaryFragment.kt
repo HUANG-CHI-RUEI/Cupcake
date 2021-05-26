d06c8a260242fa950d59a29b51768db3427fa697
HUANG-CHI-RUEI <pei4b066@gmail.com>
2021-05-26 20:37:55 +0800

    Learn about tasks and back stack(cancel button)

--- app/src/main/java/com/example/cupcake/SummaryFragment.kt
+++ app/src/main/java/com/example/cupcake/SummaryFragment.kt
@@ -22,6 +22,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentSummaryBinding
 import com.example.cupcake.model.OrderViewModel
 
@@ -61,10 +63,11 @@ class SummaryFragment : Fragment() {
         Toast.makeText(activity, "Send Order", Toast.LENGTH_SHORT).show()
     }
 
-    /**
-     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
-     * is being removed. As a result, clear out the binding object.
-     */
+   fun cancelOrder() {
+       sharedViewModel.resetOrder()
+       findNavController().navigate(R.id.action_summaryFragment_to_startFragment)
+   }
+
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
